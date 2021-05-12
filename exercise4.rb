a126e052072b238414607a0599843e91f9d8aa30
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-05-12 18:46:29 +0530

    Palindrome

---
+++ exercise4.rb
@@ -0,0 +1,19 @@
+puts "Please provide an input"
+input_string = gets.chomp 
+class String
+  def palindrome?
+    self == self.reverse
+  end
+end
+
+if input_string == ''
+  puts 'Please provide an input'
+  input_string = gets.chomp
+end
+if input_string.palindrome?
+  puts 'Input string is a palindrome'
+else
+  puts 'Input string is not a palindrome'
+end
+
+
