bfd2e0d5da4acdb34808bd9362c1299f2ee792ad
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-05-12 18:38:31 +0530

    Replace using regex

---
+++ exercise2.rb
@@ -0,0 +1,55 @@
+# Exercise: 2 - Replace using Regex
+
+# Ask the user to enter text. Replace each vowel in the text with a '*' using regular expression. Your program should accept a string as an argument and output the replaced string
+# Sample inputs
+# [name]
+# 1
+# [/name]
+# [input]
+# [/input]
+# [output]
+# Please provide an input 
+# [/output]
+
+# ===
+
+# [name]
+# 2
+# [/name]
+# [input]
+# password123 
+# [/input]
+# [output]
+# p*ssw*rd123 
+# [/output]
+
+# ===
+
+# [name]
+# 3
+# [/name]
+# [input]
+# "good morning"
+# [/input]
+# [output]
+# g**d m*rn*ng 
+# [/output]
+
+# ===
+
+# [name]
+# 4
+# [/name]
+# [input]
+# "hello everyone"
+# [/input]
+# [output]
+# h*ll* *v*ry*n* 
+# [/output]
+
+
+
+
+puts "Enter a string"
+s = gets.downcase
+puts s.gsub(/[aeiou]/,"*")
