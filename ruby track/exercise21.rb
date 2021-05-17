603e665d7f558d2909f5a176c6de5c368093a4d4
Mayank Gupta <52233220+Mayank0307@users.noreply.github.com>
2021-05-17 17:02:51 +0530

    Update exercise21.rb

--- ruby track/exercise21.rb
+++ ruby track/exercise21.rb
@@ -1,9 +1,9 @@
 def fact(n)
-    if n==0
-        puts 1
-    else
-        (1..n).reduce(:*)
-    end   
+  if n==0
+    puts 1
+  else
+    (1..n).reduce(:*)
+  end   
 end
 
 raise ArgumentError, "Negative number entered" if (ARGV[0].to_i < 0)
